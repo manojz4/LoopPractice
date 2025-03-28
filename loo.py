4e4cd90e346e2972b1fbfc37b259dd801ec14e9a
manojz4 <manoj444padvi@gmail.com>
2025-03-28 11:54:52 +0530

    test loop

---
+++ loo.py
@@ -0,0 +1,7 @@
+# Calculate the sum of the first 10 natural numbers using a for loop
+sum_of_numbers = 0
+
+for num in range(1, 11):  # Loop from 1 to 10
+    sum_of_numbers += num
+
+print("Sum using for loop:", sum_of_numbers)
