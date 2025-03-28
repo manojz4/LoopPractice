e30ff5e1963da6825327c26b45180b982c7cf98c
manojz4 <manoj444padvi@gmail.com>
2025-03-28 17:55:25 +0530

    even

---
+++ eve.py
@@ -0,0 +1,6 @@
+'''Print even numbers between 1 and 20
+Write a for loop to print all the even numbers between 1 and 20 (inclusive).
+'''
+for eve in range(1,21):
+    if eve %2 ==0:
+        print(eve) 
